98cf567da23cd4911327b620f770982cbbc231fe
moonjongjs <moonseonjong@naver.com>
2022-07-04 21:30:52 +0900

    Updates

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import KurlyComponent from './components/KurlyComponent';
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-    <KurlyComponent />
-);
